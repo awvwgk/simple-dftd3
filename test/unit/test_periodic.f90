f12ea0be2b2a216c3069ad30e897a07656541c88
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2026-05-04 12:23:24 +0200

    Fix gradient for ATM contribution (#180)

--- test/unit/test_periodic.f90
+++ test/unit/test_periodic.f90
@@ -51,10 +51,10 @@ subroutine collect_periodic(testsuite)
       & new_unittest("M06L-D3(0)", test_m06ld3zero_cyanamide), &
       & new_unittest("rPW86PBE-D3(0)", test_rpw86pbed3zero_co2), &
       & new_unittest("revSSB-D3(0)", test_revssbd3zero_cytosine), &
-      ! new_unittest("HSEsol-D3(BJ)-ATM", test_hsesold3bjatm_oxacb), &
+      & new_unittest("HSEsol-D3(BJ)-ATM", test_hsesold3bjatm_oxacb), &
       ! new_unittest("PWGGA-D3(BJ)-ATM", test_pwggad3bjatm_pyrazine), &
-      ! new_unittest("B3PW91-D3(0)-ATM", test_b3pw91d3zeroatm_urea), &
-      ! new_unittest("RPBE-D3(0)-ATM", test_rpbed3zeroatm_hexamine) &
+      & new_unittest("B3PW91-D3(0)-ATM", test_b3pw91d3zeroatm_urea), &
+      & new_unittest("RPBE-D3(0)-ATM", test_rpbed3zeroatm_hexamine), &
       & new_unittest("revPBE-D3(BJ)", test_revpbed3bj_1d), &
       & new_unittest("BP86-D3(0)", test_bp86d3zero_2d) &
       & ]
@@ -90,7 +90,7 @@ subroutine test_dftd3_gen(error, mol, param, ref)
 end subroutine test_dftd3_gen
 
 
-subroutine test_numgrad(error, mol, param)
+subroutine test_numgrad(error, mol, param, thr_in)
 
    !> Error handling
    type(error_type), allocatable, intent(out) :: error
@@ -101,37 +101,112 @@ subroutine test_numgrad(error, mol, param)
    !> Damping parameters
    class(damping_param), intent(in) :: param
 
+   !> Gradient threshold
+   real(wp), intent(in), optional :: thr_in
+
    integer :: iat, ic
    type(d3_model) :: d3
-   real(wp) :: energy, er, el, sigma(3, 3)
+   real(wp) :: energy, er, el, sigma(3, 3), thr_grad, numerr
    real(wp), allocatable :: gradient(:, :), numgrad(:, :)
    real(wp), parameter :: step = 1.0e-6_wp
+   logical :: tight
 
    allocate(gradient(3, mol%nat), numgrad(3, mol%nat))
    call new_d3_model(d3, mol)
+   thr_grad = thr2
+   if (present(thr_in)) thr_grad = thr_in
+   tight = present(thr_in)
 
    do iat = 1, mol%nat
       do ic = 1, 3
-         mol%xyz(ic, iat) = mol%xyz(ic, iat) + step
-         call get_dispersion(mol, d3, param, cutoff, er)
-         mol%xyz(ic, iat) = mol%xyz(ic, iat) - 2*step
-         call get_dispersion(mol, d3, param, cutoff, el)
-         mol%xyz(ic, iat) = mol%xyz(ic, iat) + step
-         numgrad(ic, iat) = 0.5_wp*(er - el)/step
+         if (tight) then
+            call ridders_cartesian(ic, iat, numgrad(ic, iat), numerr)
+         else
+            mol%xyz(ic, iat) = mol%xyz(ic, iat) + step
+            call get_dispersion(mol, d3, param, cutoff, er)
+            mol%xyz(ic, iat) = mol%xyz(ic, iat) - 2*step
+            call get_dispersion(mol, d3, param, cutoff, el)
+            mol%xyz(ic, iat) = mol%xyz(ic, iat) + step
+            numgrad(ic, iat) = 0.5_wp*(er - el)/step
+         end if
       end do
    end do
 
    call get_dispersion(mol, d3, param, cutoff, energy, gradient, sigma)
 
-   if (any(abs(gradient - numgrad) > thr2)) then
+   if (any(abs(gradient - numgrad) > thr_grad)) then
       call test_failed(error, "Gradient of dispersion energy does not match")
       print'(3es21.14)', gradient-numgrad
    end if
 
+contains
+
+subroutine ridders_cartesian(ic, iat, deriv, err)
+
+   integer, intent(in) :: ic, iat
+   real(wp), intent(out) :: deriv, err
+
+   integer, parameter :: max_tab = 10
+   real(wp), parameter :: initial_step = 1.0e-4_wp
+   real(wp), parameter :: step_div = sqrt(2.0_wp)
+   real(wp), parameter :: step_div_2 = step_div**2
+   integer :: iter, order
+   real(wp) :: table(max_tab, max_tab), factor, err_est, prev_deriv, prev_err
+   real(wp) :: h, x0
+
+   x0 = mol%xyz(ic, iat)
+   h = initial_step
+   err = huge(1.0_wp)
+   prev_err = err
+
+   mol%xyz(ic, iat) = x0 + h
+   call get_dispersion(mol, d3, param, cutoff, er)
+   mol%xyz(ic, iat) = x0 - h
+   call get_dispersion(mol, d3, param, cutoff, el)
+   table(1, 1) = 0.5_wp*(er - el)/h
+   deriv = table(1, 1)
+   prev_deriv = deriv
+
+   do iter = 2, max_tab
+      h = h / step_div
+      mol%xyz(ic, iat) = x0 + h
+      call get_dispersion(mol, d3, param, cutoff, er)
+      mol%xyz(ic, iat) = x0 - h
+      call get_dispersion(mol, d3, param, cutoff, el)
+      table(iter, 1) = 0.5_wp*(er - el)/h
+
+      factor = step_div_2
+      do order = 1, iter - 1
+         factor = factor * step_div_2
+         table(iter, order + 1) = (factor*table(iter, order) &
+            & - table(iter - 1, order))/(factor - 1.0_wp)
+         err_est = max(abs(table(iter, order + 1) - table(iter, order)), &
+            & abs(table(iter, order + 1) - table(iter - 1, order)))
+         if (err_est <= err) then
+            err = err_est
+            deriv = table(iter, order + 1)
+         end if
+      end do
+
+      if (abs(table(iter, iter) - table(iter - 1, iter - 1)) >= 2*err &
+         & .and. iter > 2) then
+         deriv = prev_deriv
+         err = prev_err
+         exit
+      end if
+
+      prev_deriv = deriv
+      prev_err = err
+   end do
+
+   mol%xyz(ic, iat) = x0
+
+end subroutine ridders_cartesian
+
 end subroutine test_numgrad
 
 
-subroutine test_numsigma(error, mol, param)
+subroutine test_numsigma(error, mol, param, thr_in)
 
    !> Error handling
    type(error_type), allocatable, intent(out) :: error
@@ -142,43 +217,130 @@ subroutine test_numsigma(error, mol, param)
    !> Damping parameters
    class(damping_param), intent(in) :: param
 
+   !> Strain derivative threshold
+   real(wp), intent(in), optional :: thr_in
+
    integer :: ic, jc
    type(d3_model) :: d3
    real(wp) :: energy, er, el, sigma(3, 3), eps(3, 3), numsigma(3, 3), lattice(3, 3)
+   real(wp) :: thr_sigma, numerr
    real(wp), allocatable :: gradient(:, :), xyz(:, :)
    real(wp), parameter :: unity(3, 3) = reshape(&
       & [1, 0, 0, 0, 1, 0, 0, 0, 1], shape(unity))
    real(wp), parameter :: step = 1.0e-7_wp
+   logical :: tight
 
    allocate(gradient(3, mol%nat), xyz(3, mol%nat))
    call new_d3_model(d3, mol)
+   thr_sigma = thr3
+   if (present(thr_in)) thr_sigma = thr_in
+   tight = present(thr_in)
 
    eps(:, :) = unity
    xyz(:, :) = mol%xyz
    lattice(:, :) = mol%lattice
    do ic = 1, 3
       do jc = 1, 3
-         eps(jc, ic) = eps(jc, ic) + step
-         mol%xyz(:, :) = matmul(eps, xyz)
-         mol%lattice(:, :) = matmul(eps, lattice)
-         call get_dispersion(mol, d3, param, cutoff, er)
-         eps(jc, ic) = eps(jc, ic) - 2*step
-         mol%xyz(:, :) = matmul(eps, xyz)
-         mol%lattice(:, :) = matmul(eps, lattice)
-         call get_dispersion(mol, d3, param, cutoff, el)
-         eps(jc, ic) = eps(jc, ic) + step
-         mol%xyz(:, :) = xyz
-         mol%lattice(:, :) = lattice
-         numsigma(jc, ic) = 0.5_wp*(er - el)/step
+         if (tight) then
+            call ridders_strain(ic, jc, numsigma(jc, ic), numerr)
+         else
+            eps(jc, ic) = eps(jc, ic) + step
+            mol%xyz(:, :) = matmul(eps, xyz)
+            mol%lattice(:, :) = matmul(eps, lattice)
+            call get_dispersion(mol, d3, param, cutoff, er)
+            eps(jc, ic) = eps(jc, ic) - 2*step
+            mol%xyz(:, :) = matmul(eps, xyz)
+            mol%lattice(:, :) = matmul(eps, lattice)
+            call get_dispersion(mol, d3, param, cutoff, el)
+            eps(jc, ic) = eps(jc, ic) + step
+            mol%xyz(:, :) = xyz
+            mol%lattice(:, :) = lattice
+            numsigma(jc, ic) = 0.5_wp*(er - el)/step
+         end if
       end do
    end do
 
    call get_dispersion(mol, d3, param, cutoff, energy, gradient, sigma)
 
-   if (any(abs(sigma - numsigma) > thr3)) then
+   if (any(abs(sigma - numsigma) > thr_sigma)) then
       call test_failed(error, "Strain derivatives do not match")
       print'(3es21.14)', sigma-numsigma
    end if
+
+contains
+
+subroutine ridders_strain(ic, jc, deriv, err)
+
+   integer, intent(in) :: ic, jc
+   real(wp), intent(out) :: deriv, err
+
+   integer, parameter :: max_tab = 10
+   real(wp), parameter :: initial_step = 5.0e-8_wp
+   real(wp), parameter :: step_div = sqrt(2.0_wp)
+   real(wp), parameter :: step_div_2 = step_div**2
+   integer :: iter, order
+   real(wp) :: table(max_tab, max_tab), factor, err_est, prev_deriv, prev_err
+   real(wp) :: h
+
+   h = initial_step
+   err = huge(1.0_wp)
+   prev_err = err
+
+   call strain_energy(ic, jc, h, er)
+   call strain_energy(ic, jc, -h, el)
+   table(1, 1) = 0.5_wp*(er - el)/h
+   deriv = table(1, 1)
+   prev_deriv = deriv
+
+   do iter = 2, max_tab
+      h = h / step_div
+      call strain_energy(ic, jc, h, er)
+      call strain_energy(ic, jc, -h, el)
+      table(iter, 1) = 0.5_wp*(er - el)/h
+
+      factor = step_div_2
+      do order = 1, iter - 1
+         factor = factor * step_div_2
+         table(iter, order + 1) = (factor*table(iter, order) &
+            & - table(iter - 1, order))/(factor - 1.0_wp)
+         err_est = max(abs(table(iter, order + 1) - table(iter, order)), &
+            & abs(table(iter, order + 1) - table(iter - 1, order)))
+         if (err_est <= err) then
+            err = err_est
+            deriv = table(iter, order + 1)
+         end if
+      end do
+
+      if (abs(table(iter, iter) - table(iter - 1, iter - 1)) >= 2*err &
+         & .and. iter > 2) then
+         deriv = prev_deriv
+         err = prev_err
+         exit
+      end if
+
+      prev_deriv = deriv
+      prev_err = err
+   end do
+
+   mol%xyz(:, :) = xyz
+   mol%lattice(:, :) = lattice
+
+end subroutine ridders_strain
+
+subroutine strain_energy(ic, jc, step, energy)
+
+   integer, intent(in) :: ic, jc
+   real(wp), intent(in) :: step
+   real(wp), intent(out) :: energy
+   real(wp) :: strain(3, 3)
+
+   strain(:, :) = unity
+   strain(jc, ic) = strain(jc, ic) + step
+   mol%xyz(:, :) = matmul(strain, xyz)
+   mol%lattice(:, :) = matmul(strain, lattice)
+   call get_dispersion(mol, d3, param, cutoff, energy)
+
+end subroutine strain_energy
 
 end subroutine test_numsigma
 
@@ -340,7 +502,7 @@ subroutine test_hsesold3bjatm_oxacb(error)
 
    call get_structure(mol, "X23", "oxacb")
    call new_rational_damping(param, inp)
-   call test_numgrad(error, mol, param)
+   call test_numgrad(error, mol, param, 1.0e-11_wp)
 
 end subroutine test_hsesold3bjatm_oxacb
 
@@ -358,7 +520,7 @@ subroutine test_pwggad3bjatm_pyrazine(error)
 
    call get_structure(mol, "X23", "pyrazine")
    call new_rational_damping(param, inp)
-   call test_numsigma(error, mol, param)
+   call test_numsigma(error, mol, param, 1.0e-8_wp)
 
 end subroutine test_pwggad3bjatm_pyrazine
 
@@ -376,7 +538,7 @@ subroutine test_b3pw91d3zeroatm_urea(error)
 
    call get_structure(mol, "X23", "urea")
    call new_zero_damping(param, inp)
-   call test_numgrad(error, mol, param)
+   call test_numgrad(error, mol, param, 1.0e-11_wp)
 
 end subroutine test_b3pw91d3zeroatm_urea
 
@@ -394,7 +556,7 @@ subroutine test_rpbed3zeroatm_hexamine(error)
 
    call get_structure(mol, "X23", "hexamine")
    call new_zero_damping(param, inp)
-   call test_numsigma(error, mol, param)
+   call test_numsigma(error, mol, param, 1.0e-8_wp)
 
 end subroutine test_rpbed3zeroatm_hexamine
 
