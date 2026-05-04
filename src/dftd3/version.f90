8c24594dff776b62815e29d8b82e35a74ca2eaf4
Sebastian Ehlert <sehlert@microsoft.com>
2026-05-04 12:28:29 +0200

    Update to version 1.3.2

--- src/dftd3/version.f90
+++ src/dftd3/version.f90
@@ -24,10 +24,10 @@ module dftd3_version
 
 
    !> String representation of the s-dftd3 version
-   character(len=*), parameter :: dftd3_version_string = "1.3.1"
+   character(len=*), parameter :: dftd3_version_string = "1.3.2"
 
    !> Numeric representation of the s-dftd3 version
-   integer, parameter :: dftd3_version_compact(3) = [1, 3, 1]
+   integer, parameter :: dftd3_version_compact(3) = [1, 3, 2]
 
 
 contains
